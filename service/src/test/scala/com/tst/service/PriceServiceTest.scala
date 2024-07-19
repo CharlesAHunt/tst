9fe914c5a1296c5f10589b2c012817385e9f02ce
Charles Hunt <charlesalberthunt@gmail.com>
2024-07-19 03:13:11 -0700

    add test case for Left

--- service/src/test/scala/com/tst/service/PriceServiceTest.scala
+++ service/src/test/scala/com/tst/service/PriceServiceTest.scala
@@ -1,6 +1,7 @@
 package com.tst.service
 
 import com.tst.fixtures.TestOutputData.rateGroups
+import com.tst.service.models.RatesAndPrices.CabinPrice
 import com.tst.service.services.PriceService
 
 object PriceServiceTest extends TestResource {
@@ -15,6 +16,13 @@ object PriceServiceTest extends TestResource {
     for {
       e <- PriceService.getBestGroupPrices(InputData.rateGroups, InputData.cabinPrices).value
     } yield expect(e.toOption.getOrElse(List.empty).nonEmpty)
+  }
+
+  test("Test price service returns a failure for a bad rate group input") { _ =>
+    val invalid = CabinPrice("invalid", "invalid", 0.0)
+    for {
+      e <- PriceService.getBestGroupPrices(InputData.rateGroups, InputData.cabinPrices.:+(invalid)).value
+    } yield expect(e.isLeft)
   }
 
   test("Test price return empty for empty input") { _ =>
